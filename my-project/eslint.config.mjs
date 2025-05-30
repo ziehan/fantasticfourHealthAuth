d7b919450faf19e8a06dd38b0cee600aaf781c32
ZulfahmiFjr <mccraft874@gmail.com>
2025-05-31 00:47:33 +0700

    add eslintrc.js

--- my-project/eslint.config.mjs
+++ my-project/eslint.config.mjs
@@ -9,8 +9,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
 
-export default eslintConfig;
+// export default eslintConfig;
+
+ 
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
+ 
+export default eslintConfig
