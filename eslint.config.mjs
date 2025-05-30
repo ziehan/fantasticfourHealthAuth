f14049c7f490ba8032a221b8a91c89f26f68480c
ZulfahmiFjr <mccraft874@gmail.com>
2025-05-31 01:45:14 +0700

    Disable no-unused-vars rule for now

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,7 @@ const eslintConfig = [
   ...compat.config({
     extends: ['next'],
     rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   }),
 ]
