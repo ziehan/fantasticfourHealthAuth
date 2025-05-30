6b993b09f4b07ebd9331703ea2671f9ff2121a5f
ZulfahmiFjr <mccraft874@gmail.com>
2025-05-30 15:25:22 +0700

    zul

--- my-project/src/app/page.tsx
+++ my-project/src/app/page.tsx
@@ -3,6 +3,8 @@ import Hero from '../sections/hero';
 import About from '../sections/about';
 import Footer from '../sections/footer';
 
+
+
 export default function HomePage() {
   return (
     <div className="text-foreground antialiased selection:bg-primary-DEFAULT/30 selection:text-primary-DEFAULT">
