6d5033d1c154e8e10fa0fe9ffa860a19e78ab5d3
ZulfahmiFjr <mccraft874@gmail.com>
2025-05-31 01:10:25 +0700

    Move project to root

--- my-project/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'off'
-  }
-};
