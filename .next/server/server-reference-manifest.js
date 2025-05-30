460ab552230cfe6995ec1157148efa16cf0574a8
ZulfahmiFjr <mccraft874@gmail.com>
2025-05-31 01:24:53 +0700

    Remove .next build folder from git tracking

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UXPFRSQ/b+Fvt6mpIJyQu782oSdd0ftGEF6qaBbeu+o=\"\n}"
--- .next/static/chunks/src_app_page_tsx_8450df25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_sections_444a7b3c._.js",
-  "static/chunks/node_modules_864caa74._.js"
-],
-    source: "dynamic"
-});
